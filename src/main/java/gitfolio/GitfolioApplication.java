33532058ace330c3a800942b669f88b64b499808
samiksha-Diwate <samikshadiwate2005@gmail.com>
2026-05-22 18:47:07 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/gitfolio/GitfolioApplication.java
@@ -0,0 +1,13 @@
+package gitfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gitfolio/GitfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package gitfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
