a987f8056bc360a816808d497c6b79e1a31c0e38
samiksha-Diwate <samikshadiwate2005@gmail.com>
2026-05-23 15:08:57 +0530

    Fixed GitHub user model for repository count

--- src/main/java/gitfolio/model/GitHubUser.java
+++ src/main/java/gitfolio/model/GitHubUser.java
@@ -1,62 +1,21 @@
 package gitfolio.model;
 
+import lombok.Data;
+
+@Data
 public class GitHubUser {
 
     private String login;
+
     private String name;
-    private String bio;
-    private int followers;
-    private int following;
+
     private String avatar_url;
 
-    public GitHubUser() {
-    }
+    private String bio;
 
-    public String getLogin() {
-        return login;
-    }
+    private int followers;
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+    private int following;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public int getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(int followers) {
-        this.followers = followers;
-    }
-
-    public int getFollowing() {
-        return following;
-    }
-
-    public void setFollowing(int following) {
-        this.following = following;
-    }
-
-    public String getAvatar_url() {
-        return avatar_url;
-    }
-
-    public void setAvatar_url(String avatar_url) {
-        this.avatar_url = avatar_url;
-    }
+    private int public_repos;
 }
